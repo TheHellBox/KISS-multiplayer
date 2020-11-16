bc49ec7ef1582cd3bc067653ca990907f19be829
TheHellBox <thehellbox11@gmail.com>
2020-11-16 22:46:15 +0500

    Micro optimization on velocity apply function

--- KISSMultiplayer/lua/vehicle/extensions/kiss_mp/kiss_vehicle.lua
+++ KISSMultiplayer/lua/vehicle/extensions/kiss_mp/kiss_vehicle.lua
@@ -7,13 +7,15 @@ local nodes_per_frame = 64
 local node_pos_thresh = (32 * 32)
 
 local function kissInit()
+  local force = obj:getPhysicsFPS()
+  force = vec3(force, force, force):toFloat3()
   for _, node in pairs(v.data.nodes) do
     local mass = obj:getNodeMass(node.cid)
     table.insert(
       nodes,
       {
         node.cid,
-        vec3(mass, mass, mass):toFloat3(),
+        vec3(mass, mass, mass):toFloat3() * force,
         true
       }
     )
@@ -50,14 +52,12 @@ end
 
 local function apply_full_velocity(x, y, z, pitch, roll, yaw)
   local velocity = vec3(x, y, z):toFloat3()
-  local force = obj:getPhysicsFPS()
-  local force = vec3(force, force, force):toFloat3()
   local rot = vec3(pitch, roll, yaw):rotated(quat(obj:getRotation())):toFloat3()
   for k=1, #nodes do
     local node = nodes[k]
     if node[3] then
       local node_position = obj:getNodePosition(node[1])
-      local force = (velocity + node_position:cross(rot)) * node[2] * force
+      local force = (velocity + node_position:cross(rot)) * node[2]
       obj:applyForceVector(node[1], force)
     end
   end
