113fb48f475a20a05a95394373895ea50f71c0d5
TheHellBox <thehellbox11@gmail.com>
2020-11-26 07:35:43 +0500

    Fix getCurrentVehicle

--- kissmp-server/src/vehicle/mod.rs
+++ kissmp-server/src/vehicle/mod.rs
@@ -86,6 +86,7 @@ impl crate::Server {
     pub async fn set_current_vehicle(&mut self, client_id: u32, vehicle_id: u32) {
         let connection = self.connections.get_mut(&client_id).unwrap();
         connection.client_info.current_vehicle = vehicle_id;
+        let _ = self.update_lua_connections();
     }
 
     pub fn get_server_id_from_game_id(&self, client_id: u32, game_id: u32) -> Option<u32> {
