3490562729bdc95405eba491bed5be1532671069
Dummiesman <aaro4130@gmail.com>
2022-06-29 16:43:56 -0400

    Fix input lua error due to nil steering angle

--- KISSMultiplayer/lua/vehicle/extensions/kiss_mp/kiss_input.lua
+++ KISSMultiplayer/lua/vehicle/extensions/kiss_mp/kiss_input.lua
@@ -16,9 +16,9 @@ local function apply(data)
   local data = jsonDecode(data)
   input.event("throttle", data.throttle_input, 1)
   input.event("brake", data.brake_input, 2)
-  input.event("steering", data.steering_input, 2)
   input.event("parkingbrake", data.parkingbrake, 2)
   input.event("clutch", data.clutch, 1)
+  input.event("steering", data.steering_input, 2, 0, 0)
 end
 
 local function kissUpdateOwnership(owned)
