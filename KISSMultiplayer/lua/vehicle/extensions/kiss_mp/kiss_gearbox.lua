dbfb5690087ec346691dfdcb63addff018998d9c
Dummiesman <aaro4130@gmail.com>
2023-02-01 17:24:10 -0500

    Re-do gearbox code, much more reliable

--- KISSMultiplayer/lua/vehicle/extensions/kiss_mp/kiss_gearbox.lua
+++ KISSMultiplayer/lua/vehicle/extensions/kiss_mp/kiss_gearbox.lua
@@ -1,78 +1,91 @@
 local M = {}
-M.arcade = true
 
+local mainController = nil
 local gearbox = nil
-local gearbox_is_dct = false
-local gearbox_is_manual = false
+local gearbox_is_manual_type = false
+local gearbox_is_sequential = false
+local vehicle_is_electric = false
 
-local function gearboxBehaviorChanged(behavior)
-  M.arcade = behavior == "arcade"
-end
+local sequential_lock = false
+local ownership = false
+local cooldown_timer = 0
 
 local function set_gear_indices(indices)
-  if gearbox_is_dct then
-    gearbox:setGearIndex1(indices[1])
-    gearbox:setGearIndex2(indices[2])
-  else
-    gearbox:setGearIndex(indices[1])
+  if mainController and cooldown_timer < 0 then
+    local index = indices[1]
+    local canShift = true
+    
+    -- there's a neutralRejectTimer that will lock sequentials into neutral if we try it more than once
+    -- possibly a game bug
+    if sequential_lock then
+      canShift = false
+    elseif index == 0 and gearbox_is_sequential then
+      sequential_lock = true
+    end
+    
+    if canShift then
+      mainController.shiftToGearIndex(index, true) -- true for ignoring sequential bounds
+    end
   end
 end
 
 local function get_gear_indices()
-  if gearbox_is_dct then
-    return {gearbox.gearIndex1, gearbox.gearIndex2}
-  else
-    return {gearbox.gearIndex, 0}
-  end
-end
-
-local function send()
-  if not gearbox then return end
+  local index = electrics.values.gearIndex
   
-  local data = {
-    vehicle_id = obj:getID() or 0,
-    arcade = M.arcade or false,
-    lock_coef = gearbox.lockCoef or 0,
-    mode = gearbox.mode or "none",
-    gear_indices = get_gear_indices() or {0, 0},
-  }
-  obj:queueGameEngineLua("network.send_messagepack(3, false, \'"..jsonEncode(data).."\')")
+  -- convert gearIndex to values that shiftToGearIndex accepts
+  if index == nil then index = 0 end
+  if not gearbox_is_manual_type then
+    if type(electrics.values.gear) == 'string' and string.sub(electrics.values.gear, 1, 1) == 'M' then
+      index = 6 -- M1 is the best we can do
+    elseif electrics.values.gear == "P" then     
+      index = 1 -- park
+    elseif index >= 1 then
+      index = 2 -- drive
+    end
+  end
+  
+  return {index, 0}
 end
 
 local function get_gearbox_data()
-  if not gearbox then
-    return {
-      vehicle_id = obj:getID(),
-      arcade = true,
-      lock_coef = 0,
-      mode = "none",
-      gear_indices = {0, 0}
-    }
-  end
   local data = {
     vehicle_id = obj:getID(),
-    arcade = M.arcade,
-    lock_coef = gearbox.lockCoef,
-    mode = gearbox.mode or "none",
+    lock_coef = gearbox and gearbox.lockCoef or 0,
+    mode = gearbox and gearbox.mode or "none",
     gear_indices = get_gear_indices(),
+    arcade = false
   }
   return data
 end
 
 local function apply(data)
-  if not gearbox then return end
   local data = jsonDecode(data)
   set_gear_indices(data.gear_indices)
-  if not gearbox_is_manual and data.mode ~= "none" then
-    gearbox:setMode(data.mode)
+end
+
+local function updateGFX(dt)
+  if ownership then return end
+  if cooldown_timer > 0 then
+    cooldown_timer = cooldown_timer - clamp(dt, 0, 0.02)
+    return
+  end
+  if sequential_lock and electrics.values.gearIndex == 0 then
+    sequential_lock = false
   end
 end
 
+local function onReset()
+  cooldown_timer = 0.2
+  sequential_lock = false
+end
+
 local function kissInit()
+  mainController = controller.mainController
+  vehicle_is_electric = tableSize(powertrain.getDevicesByType("electricMotor")) > 0
   gearbox = powertrain.getDevice("gearbox")
   
   -- Search for a gearbox if one wasn't found
-  if not gearbox then
+  if not gearbox and not vehicle_is_electric then
     local devices = powertrain.getDevices()
     for _, device in pairs(devices) do
       if device.deviceCategories.gearbox and gearbox == nil then
@@ -80,22 +93,17 @@ local function kissInit()
       end
     end
   end
-  if not gearbox then return end
   
-  -- Reject CVT gearboxes because they don't have gears
-  if gearbox.type == "cvtGearbox" then 
-    gearbox = nil
-    return
+  if gearbox then
+    gearbox_is_manual_type = (gearbox.type == "manualGearbox" or gearbox.type == "sequentialGearbox")
+    gearbox_is_sequential = gearbox.type == "sequentialGearbox"
   end
-  
-  gearbox_is_dct = gearbox.type == "dctGearbox"
-  gearbox_is_manual = gearbox.type == "manualGearbox" 
 end
 
 local function kissUpdateOwnership(owned)
+  ownership = owned
   if owned then return end
-  if not gearbox then return end
-  if gearbox.type == "manualGearbox" then
+  if gearbox and gearbox_is_manual then
     gearbox.gearDamageThreshold = math.huge
   end
 end
@@ -103,7 +111,8 @@ end
 M.send = send
 M.apply = apply
 M.get_gearbox_data = get_gearbox_data
-M.gearboxBehaviorChanged = gearboxBehaviorChanged
+M.updateGFX = updateGFX
+M.onReset = onReset
 M.kissInit = kissInit
 M.kissUpdateOwnership = kissUpdateOwnership
 
