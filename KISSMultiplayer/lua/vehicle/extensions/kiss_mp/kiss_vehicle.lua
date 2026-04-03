9cf1e2537237fd65a37491935a6250899073ce4b
DaddelZeit <141176220+DaddelZeit@users.noreply.github.com>
2026-04-03 17:52:39 +0200

    Update to zero garbage object handling (4)

--- KISSMultiplayer/lua/vehicle/extensions/kiss_mp/kiss_vehicle.lua
+++ KISSMultiplayer/lua/vehicle/extensions/kiss_mp/kiss_vehicle.lua
@@ -68,9 +68,6 @@ local function update_eligible_nodes()
 end
 
 local function update_transform_info()
-  local r = quat(obj:getRotation())
-  local p = obj:getPosition()
-
   local throttle_input = electrics.values.throttle_input or 0
   local brake_input = electrics.values.brake_input or 0
   if electrics.values.gearboxMode == "arcade" and electrics.values.gearIndex < 0 then
@@ -87,8 +84,8 @@ local function update_transform_info()
   }
   local gearbox = kiss_gearbox.get_gearbox_data()
   local transform = {
-    position  = {p.x, p.y, p.z},
-    rotation  = {r.x, r.y, r.z, r.w},
+    position  = {obj:getPositionXYZ()},
+    rotation  = {obj:getRotation()},
     input = input,
     gearbox = gearbox,
     vel_pitch = obj:getPitchAngularVelocity(),
@@ -98,9 +95,11 @@ local function update_transform_info()
   obj:queueGameEngineLua("kisstransform.push_transform("..obj:getID()..", " .. string.format("%q", jsonEncode(transform)) .. ")")
 end
 
+local velocity = vec3(x, y, z)
+local force = vec3(0, 0, 0)
+local angular_velocity = vec3()
 local function apply_linear_velocity(x, y, z)
-  local velocity = vec3(x, y, z)
-  local force = float3(0, 0, 0)
+  velocity:set(x, y, z)
   for k=1, #nodes do
     local node = nodes[k]
     if node[3] then
@@ -111,23 +110,31 @@ local function apply_linear_velocity(x, y, z)
   end
 end
 
+local object_rotation = quat()
+local node_position = vec3()
 local function apply_linear_velocity_ang_torque(x, y, z, pitch, roll, yaw)
-  local velocity = vec3(x, y, z)
-  local nodes = nodes
+  velocity:set(x, y, z)
   -- 0.1 seems like the safe value we can use for low velocities
   -- NOTE: Doesn't work as well as expected
-  if velocity:length() < 0.01 then
+  --if velocity:length() < 0.01 then
     --nodes = ref_nodes
-  end
-  local rot = vec3(pitch, roll, yaw):rotated(quat(obj:getRotation()))
-  local node_position = vec3()
-  local force = float3(0, 0, 0)
+  --end
+
+  object_rotation:set(obj:getRotation())
+  angular_velocity:set(pitch, roll, yaw)
+  angular_velocity:setRotate(object_rotation)
+
   for k=1, #nodes do
     local node = nodes[k]
     if node[3] then
+      -- as there is no getNodePositionXYZ, this cannot be further optimized
+      -- TODO: check if future game updates add this, would be a drop-in replacement
       node_position:set(obj:getNodePosition(node[1]))
-      local result = (velocity + node_position:cross(rot)) * node[2]
-      force:set(result.x, result.y, result.z)
+
+      -- force = (velocity + node_position:cross(angular_velocity)) * node[2]
+      force:setCross(node_position, angular_velocity)
+      force:setAdd(velocity)
+      force:setScaled(node[2])
       obj:applyForceVector(node[1], force)
     end
   end
@@ -135,11 +142,9 @@ end
 
 local function send_vehicle_config()
   local config = v.config
-  local r = quat(obj:getRotation())
-  local p = obj:getPosition()
   local data = {
-    position = {p.x, p.y, p.z},
-    rotation = {r.x, r.y, r.z, r.w},
+    position = {obj:getPositionXYZ()},
+    rotation = {obj:getRotation()},
   }
   obj:queueGameEngineLua("vehiclemanager.send_vehicle_config_inner("..obj:getID()..", " .. string.format("%q", jsonEncode(config)) .. ", " .. string.format("%q", jsonEncode(data)) .. ")")
 end
