432424def108da1cff15b05faa03f5fb187f14e3
Vlad118 <vlad.stratulat1@gmail.com>
2026-04-12 15:56:06 +0100

    added build for placeholder icons for windows executable

---
+++ kissmp-server/build.rs
@@ -0,0 +1,11 @@
+#[cfg(windows)]
+fn main() {
+    if std::env::var("CARGO_FEATURE_BUILD_ICON").is_ok() {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("../assets/server_icon.ico");
+        res.compile().unwrap();
+    }
+}
+
+#[cfg(not(windows))]
+fn main() {}
