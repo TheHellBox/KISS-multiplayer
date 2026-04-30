e1b2397e317c349c28030765be9f70a354c57668
Moritz Riede <moritz@mriede.com>
2026-05-01 00:34:07 +0200

    fix: suppress local input on remote vehicles

--- KISSMultiplayer/lua/vehicle/extensions/kiss_mp/kiss_input.lua
+++ KISSMultiplayer/lua/vehicle/extensions/kiss_mp/kiss_input.lua
@@ -1,21 +1,59 @@
 local M = {}
 
+local ownership = true
+local local_inputs_disabled = false
+local remote_input_source = "KissMP"
+local controlled_inputs = {
+  throttle = true,
+  brake = true,
+  parkingbrake = true,
+  clutch = true,
+  steering = true,
+}
+
+local function set_local_input_allowed(allowed)
+  if not input or not input.setAllowedInputSource then return end
+  for input_name in pairs(controlled_inputs) do
+    input.setAllowedInputSource(input_name, "local", allowed)
+    input.setAllowedInputSource(input_name, remote_input_source, not allowed)
+  end
+  local_inputs_disabled = not allowed
+end
+
 local function apply(data)
+  if ownership then return end
   local data = jsonDecode(data)
-  input.event("throttle", data.throttle_input, 1)
-  input.event("brake", data.brake_input, 2)
-  input.event("parkingbrake", data.parkingbrake, 2)
-  input.event("clutch", data.clutch, 1)
-  input.event("steering", data.steering_input, 2, 0, 0)
+  input.event("throttle", data.throttle_input, 1, nil, nil, nil, remote_input_source)
+  input.event("brake", data.brake_input, 2, nil, nil, nil, remote_input_source)
+  input.event("parkingbrake", data.parkingbrake, 2, nil, nil, nil, remote_input_source)
+  input.event("clutch", data.clutch, 1, nil, nil, nil, remote_input_source)
+  input.event("steering", data.steering_input, 2, 0, 0, nil, remote_input_source)
 end
 
 local function kissUpdateOwnership(owned)
-  if owned then return end
-  hydros.enableFFB = false
-  hydros.onFFBConfigChanged(nil)
+  ownership = owned and true or false
+  if ownership then
+    if local_inputs_disabled then
+      set_local_input_allowed(true)
+    end
+    return
+  end
+
+  set_local_input_allowed(false)
+  if hydros then
+    hydros.enableFFB = false
+    hydros.onFFBConfigChanged(nil)
+  end
+end
+
+local function updateGFX()
+  if not ownership and not local_inputs_disabled then
+    set_local_input_allowed(false)
+  end
 end
 
 M.apply = apply
+M.updateGFX = updateGFX
 
 M.kissUpdateOwnership = kissUpdateOwnership
 
